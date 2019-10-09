a4fbd1c613d7be48a8c850693e431ef674797332
CassioJhones <56178855+CassioJhones@users.noreply.github.com>
2019-10-09 03:50:07 -0300

    Exercicio 5
    Leitor de antecessor e sucessor

---
+++ 005.py
@@ -0,0 +1,9 @@
+from time import sleep
+
+'''Exercício 005: Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor.'''
+
+x = int(input('Digite um numero: '))
+suce = x+1
+ante = x-1
+print('o Sucessor é {}, o Antecessor é {}'.format(suce, ante))
+sleep(3) #AGUARDAR 3 SECUNDOS PARA FECHAR PROGRAMA
