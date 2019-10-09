902404e8ba389e8c6d842482bb9998ae221b7571
Cassio Jhones <cassio.jpzero@gmail.com>
2019-10-09 15:22:40 -0300

    Advinhar
    adicionado função while

--- 028_ADVINHAR.py
+++ 028_ADVINHAR.py
@@ -1,19 +1,22 @@
-#  Created by Cássio Jhones on 15/03/19 16:46.
-
 '''Exercício 028: Faça o computador "pensar" em um número inteiro entre 0 e 5 e peça para o usuário
-tentar descobrir qual foi o número escolhido pelo computador. Escreva na tela se o usuário venceu ou perdeu.'''
+tentar descobrir qual foi o número escolhido pelo computador. Escreva na tela se o usuário venceu ou perdeu.'''    
 
 from random import randint
 from time import sleep
-PC = randint(0, 5)
-print('-=-'*15)
-print('Pensei em um numero de 0 a 5, tente acertar: ')
-print('-=-'*15)
 
-USER = int(input('Pensei no: '))
-print('\nPROCESSANDO...')
-sleep(0.5)
-if PC == USER:
-    print('Voce acertou, Parabens!')
-else:
-    print('Voce Errou')
+PC = randint(0, 10)
+USER = 0
+
+while PC != USER:
+    print('-=-'*15)
+    #print(PC) # NAO MOSTRAR PARA O USUARIO
+    print('Pensei em um numero de 0 a 10, tente acertar: ')
+    print('-=-'*15)
+    
+    USER = int(input('Pensei no: '))
+    print('PROCESSANDO...')
+    sleep(0.6)
+    if PC == USER:
+        print('Voce acertou, Parabens!')
+    else:
+        print('Voce Errou')
